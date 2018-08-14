cf3840a74ea50ab0f3a1c1777fa11dccd8bed3cc
Ahmed <a.abdelaziz44@gmail.com>
2018-08-14 02:23:12 +0200

    Added the slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-    // init Custom Filter
+    // init the filter
     var $btns = $('.filter-list a').click(function() {
         if (this.id == 'all') {
             $('.projects-grid > .projects-grid__item').fadeIn(500);
@@ -10,6 +10,36 @@ $(document).ready(function(){
         }
         $btns.parent().removeClass('active');
         $(this).parent().addClass('active');
-    });
+    });    
 
 });
+
+// init the slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("project-slider__item");
+    if (n > slides.length) {slideIndex = 1;} 
+    if (n < 1) {slideIndex = slides.length;}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active', 'inactive');
+        if(i > (slideIndex-1)) {
+            slides[slideIndex].classList.add('inactive');
+        } else if(i === slides.length - 1) {
+            slides[0].classList.add('inactive');
+        }
+    }
+    slides[slideIndex-1].classList.add('active');
+}
